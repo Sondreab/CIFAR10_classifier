1ba7f6f8a2ded50071b873bc7c05e5dfa3b30c33
marmadsen <martinmad94@gmail.com>
2019-02-26 11:10:47 +0100

    update on weight calc

--- src/dimensions_task2.py
+++ src/dimensions_task2.py
@@ -21,12 +21,12 @@ def forward_dimensions(h, w, kernel_size, stride, padding=0):
 
 def calc_dims_and_params(parameters=0):
     h, w, c = h1, w1, c1
-    kernel_parameters = ((kernel_conv+padding_conv)**2)*c + 1
+    parameters += (((kernel_conv)**2)*c + 1)*num_filters
     for i in range(num_layers):
         h, w = forward_dimensions(h, w, kernel_conv, stride_conv, padding_conv)
         h, w = forward_dimensions(h, w, kernel_pool, stride_pool)
         #parameters += ((kernel_conv+padding_conv)**2 + 1)*num_filters*2**i
-        parameters += (kernel_parameters)*num_filters*2**i
+        parameters += (kernel_conv**2)*num_filters*(2**i)
         print("After layer ",i)
         print("{} x {} x {}".format(h, w, num_filters*2**i))
     print("Parameters: {}".format(parameters))
